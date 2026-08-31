560204362bb6b4036204de0f413b2881b5d89aa6
EV-135 <velasqen@mail.uc.edu>
2026-08-31 17:33:26 -0400

    Trying to fix multiple names logic

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -6,30 +6,35 @@ import java.util.ArrayList;
 public class PersonGenerator {
     public static void main(String[] args) {
         Boolean cont = true;
-
+        String returnName = "";
         while (cont == true) {
             String prompt = ("Please enter names");
             Scanner pipe =  new Scanner(System.in);
 
 
-            String returnName = SafeInput.getNonZeroLenString(pipe, prompt);
+            returnName = SafeInput.getNonZeroLenString(pipe, prompt);
             System.out.print(returnName);
 
-            String continueNames = ("Do you want to enter another name? y/n");
-            String yesNo = SafeInput.getNonZeroLenString(pipe, continueNames);
 
-            if (continueNames.equalsIgnoreCase("n")) {
-                cont = false;
+            Boolean validYN = false;
+            while (validYN == false) {
+                String continueNames = ("Do you want to enter another name? y/n");
+                String yesNo = SafeInput.getNonZeroLenString(pipe, continueNames);
+                if (yesNo.equals("n") || yesNo.equals("N")) {
+                    cont = false;
+                    validYN = true;
+                } else if (yesNo.equals("y") || yesNo.equals("Y")) {
+                    cont = true;
+                    validYN = true;
+                } else {
+                    System.out.println(yesNo + " is not a valid input. Please type y or n");
+                    validYN = false;
+                }
+            }
 
-            } else if (continueNames.equalsIgnoreCase("y")) {
-                cont = true;
-            }
-            else{
-                System.out.println(yesNo + " is not a valid input. Please type y or n");
-            }
-            List<String> namesArray = new ArrayList<>(Arrays.asList(returnName.split(" ")));
 
         }
+        List<String> namesArray = new ArrayList<>(Arrays.asList(returnName.split(" ")));
 
 
 
